0b54bfd7af21ccead9d196bd8bd717652c18e935
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-02-13 10:52:01 +0100

    iniciado

--- public/src/utils/data.js
+++ public/src/utils/data.js
@@ -1,12 +1,12 @@
 export const PIECES = [
-  { src: '/1.jpg', id: 1 },
-  { src: '/2.jpg', id: 2 },
-  { src: '/3.jpg', id: 3 },
-  { src: '/4.jpg', id: 4 },
-  { src: '/5.jpg', id: 5 },
-  { src: '/6.jpg', id: 6 },
-  { src: '/7.jpg', id: 7 },
-  { src: '/8.jpg', id: 8 },
-  { src: '/9.jpg', id: 9 },
+  { src: 'public/1.jpg', id: 1 },
+  { src: 'public/2.jpg', id: 2 },
+  { src: 'public/3.jpg', id: 3 },
+  { src: 'public/4.jpg', id: 4 },
+  { src: 'public/5.jpg', id: 5 },
+  { src: 'public/6.jpg', id: 6 },
+  { src: 'public/7.jpg', id: 7 },
+  { src: 'public/8.jpg', id: 8 },
+  { src: 'public/9.jpg', id: 9 },
   { src: ' ', id: 9 }
 ]
